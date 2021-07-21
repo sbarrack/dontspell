1aec83c5bdb351d4a2e676dbe008f03e05f6316b
sbarrack <12619603+sbarrack@users.noreply.github.com>
2021-07-21 08:59:54 -0700

    change to less
    will change again to sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React, { FC } from 'react';
 import { Button } from 'antd';
-import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-    <Button type="primary">Button</Button>
-  </div>
+  <Button type="primary">Button</Button>
 );
 
 export default App;
